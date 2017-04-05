ba4fce3e79e14340642e03b863f987094cf19c15
jonathankamau <jonathan.kamau@andela.com>
2017-04-05 08:37:10 +0300

    added inheritance

--- models/person.py
+++ models/person.py
@@ -1,22 +1,14 @@
+from .amity import Amity
 
-class Person:
-    """add person class which allows for adding a person who needs accomodation"""
-    def __init__(self, name, status):
-        """initializes the class """
-        self.name = name
-        self.status = status
+class Person(Amity):
+    def __init__(self):
+        pass
+      #  self.fellow_list = []
+      #  self.staff_list = []
 
-    def add_person(self, name, status):
-        """ adds a person"""
-        return "added person successfully"
-
-class Fellow:
-    """ instantiates the fellow class """
-    def __init__(self, name, status):
-        """initializes the class """
-        self.name = name
-        self.status = status
-
-        def add_fellow(self, name, status):
-            """ adds fellow"""
-            return "added fellow successfully"
+        
+class Fellow(Person):
+    pass
+    
+class Staff(Person):
+    pass
