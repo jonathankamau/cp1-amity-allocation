7349c755d8b6bae0e80a19f2aa10dcbd7d04b29c
jonathankamau <jonathan.kamau@andela.com>
2017-04-13 10:28:44 +0300

    added properties for object classes

--- models/person.py
+++ models/person.py
@@ -9,7 +9,7 @@ class Person(object):
 class Fellow(Person):
     def __init__(self, firstname, lastname, role, accomodation):
         super(Fellow, self).__init__(firstname, lastname, role="FELLOW")
-        name = firstname+" "+lastname
+        name = "{} {}".format(firstname, lastname)
         self.name = name
         self.office = ''
         self.accomodation = accomodation
@@ -18,7 +18,7 @@ class Fellow(Person):
 class Staff(Person):
     def __init__(self, firstname, lastname, role):
         super(Staff, self).__init__(firstname, lastname, role="STAFF")
-        name = firstname+" "+lastname
+        name = "{} {}".format(firstname, lastname)
         self.name = name
         self.office = ''
         self.person_id = id(name)
