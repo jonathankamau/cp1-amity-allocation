a42ab1ebe938924ce653d38253bf6b8d969a8492
jonathankamau <jonathan.kamau@andela.com>
2017-04-17 19:49:54 +0300

    added more docopt methods

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 """
-This example uses docopt with the built in cmd module to demonstrate an
-interactive command application.
 Usage:
     Amity create_room <roomtype> <roomname>
     Amity add_person <firstname> <lastname> <role> [<accomodation>]
@@ -18,21 +16,32 @@ Options:
     -i, --interactive  Interactive Mode
     --o=<filename> specifies the file name
     -h, --help  Show this screen and exit.
-    
 """
 
 import sys
 import cmd
+import os
+import re
+
 from docopt import docopt, DocoptExit
+from colorama import *
+from pyfiglet import figlet_format
 from termcolor import colored, cprint
 from models.amity import Amity
 
+
+
+cprint(figlet_format("AMITY ROOM ALLOCATION".center(
+    30), font="standard"), "blue", "on_yellow", attrs=["blink"])
+cprint("\n")
 
 def docopt_cmd(func):
     """
     This decorator is used to simplify the try/except block and pass the result
     of the docopt parsing to the called action.
     """
+    
+
     def fn(self, arg):
         try:
             opt = docopt(fn.__doc__, arg)
@@ -41,7 +50,7 @@ def docopt_cmd(func):
             # The DocoptExit is thrown when the args do not match.
             # We print a message to the user and the usage block.
 
-            print('Invalid Command!')
+            print(colored('Invalid Command!', 'red'))
             print(e)
             return
 
@@ -58,6 +67,10 @@ def docopt_cmd(func):
     fn.__dict__.update(func.__dict__)
     return fn
 
+    # def start():
+    #     os.system("clear")
+    #     print(__doc__)
+
 
 class AmityApp (cmd.Cmd):
     #text = cprint('Amity Room Allocation', 'green', 'on_red')
@@ -70,52 +83,93 @@ class AmityApp (cmd.Cmd):
     @docopt_cmd
     def do_create_room(self, args):
         """Usage: create_room <roomtype> <roomname>..."""
-        print(self.amity.create_room(args['<roomtype>'], args['<roomname>']))
+        if not re.match("^[A-Za-z]*$", args['<roomtype>']):
+            print(colored("Invalid input! Please enter roomtype in string format", "red"))
+        elif not re.match("^[A-Za-z]*$", ''.join(args['<roomname>'])):
+            print(colored("Invalid input! Please enter roomname in string format", "red"))
+        else:
+            print(self.amity.create_room(args['<roomtype>'], args['<roomname>']))
 
 
     @docopt_cmd
     def do_add_person(self, args):
         """Usage: add_person <firstname> <lastname> <role> [<accomodation>]"""
-        print(self.amity.add_person(args['<firstname>'], args['<lastname>'],
-                                    args['<role>'], args['<accomodation>']))
+        if not re.match("^[A-Za-z]*$", args['<firstname>']):
+            print(colored("Invalid input! Please enter firstname in string format", "red"))
+        elif not re.match("^[A-Za-z]*$", args['<lastname>']):
+            print(colored("Invalid input! Please enter lastname in string format", "red"))
+        elif not re.match("^[A-Za-z]*$", args['<role>']):
+            print(colored("Invalid input! Please enter role in string format", "red"))
+        elif not re.match("^[A-Za-z]*$", args['<accomodation>']):
+            print(colored("Invalid input! Please enter accomodation in string format", "red"))
+        else:
+            print(self.amity.add_person(args['<firstname>'], args['<lastname>'],
+                                        args['<role>'], args['<accomodation>']))
 
     @docopt_cmd
     def do_load_people(self, args):
         """Usage: load_people [--o=<filename>]"""
-
-        print(self.amity.load_people(args['--o']))
+        if not re.match("^[A-Za-z]*$", args['--o']):
+            print(colored("Invalid input! Please enter filename in string format", "red"))
+        else:
+            print(self.amity.load_people(args['--o']))
 
     @docopt_cmd
     def do_print_room(self, args):
         """Usage: print_room <roomname>"""
-
-        print(self.amity.print_room(args['<roomname>']))
+        if not re.match("^[A-Za-z]*$", args['<roomname>']):
+            print(colored("Invalid input! Please enter room name in string format", "red"))
+        else:
+            print(self.amity.print_room(args['<roomname>']))
 
     @docopt_cmd
     def do_reallocate_person(self, args):
-        """ Usage: reallocate_person <firstname> <lastname> <new_room_name> """
-        person_identifier = "{} {}".format(args['<firstname>'], args['<lastname>'])
-        new_room = args['<new_room_name>']
-        print(self.amity.reallocate_person(person_identifier, new_room))
+        """ Usage: reallocate_person <person_identifier> <new_room_name> """
+        if not re.match("^[0-9]+$", args['<person_identifier>']):
+            print(colored("Invalid input!"+
+                          " Please enter person_identifier in ID number format", "red"))
+        elif not re.match("^[A-Za-z]*$", args['<new_room_name>']):
+            print(colored("Invalid input! Please enter the newroom name in string format", "red"))
+        else:
+            print(self.amity.reallocate_person(args
+                                               ['<person_identifier>'], args['<new_room_name>']))
 
 
     @docopt_cmd
     def do_print_allocations(self, args):
         """Usage: print_allocations [--o=<filename>]"""
 
-        self.amity.print_allocations(args['--o'])
+        self.amity.print_allocations(args)
 
     @docopt_cmd
     def do_print_unallocated(self, args):
         """Usage: print_unallocated [--o=<filename>]"""
+        self.amity.print_unallocated(args)
 
-        self.amity.print_unallocated(args['--o'])
+    @docopt_cmd
+    def do_print_all_people(self, args):
+        """Usage: print_all_people"""
+        self.amity.print_all_people(args)
+
+    @docopt_cmd
+    def do_print_all_rooms(self, args):
+        """Usage: print_all_rooms"""
+        self.amity.print_all_rooms(args)
+
+    @docopt_cmd
+    def do_delete_person(self, args):
+        """Usage: delete_person <personid>"""
+        if not re.match("^[0-9]+$", args['<personid>']):
+            print(colored("Invalid input!"+
+                          " Please enter person_identifier in ID number format", "red"))
+        else:
+            print(self.amity.delete_person(args['<personid>']))
 
     @docopt_cmd
     def do_save_state(self, args):
         """Usage: save_state [--db=sqlite_database]"""
 
-        self.amity.save_state(args['--db'])
+        self.amity.save_state(args)
 
     @docopt_cmd
     def do_load_state(self, args):
@@ -126,12 +180,15 @@ class AmityApp (cmd.Cmd):
     def do_quit(self, args):
         """Quits out of Interactive Mode."""
 
-        print('Good Bye!')
+        print(colored('Adios! Hope to see you again soon', 'green'))
         exit()
 
-opt = docopt(__doc__, sys.argv[1:])
+#opt = docopt(__doc__, sys.argv[1:])
 
-if opt['--interactive']:
+if __name__ == '__main__':
+    print(colored(__doc__, 'green', 'on_blue', attrs=['bold']))
     AmityApp().cmdloop()
+# if opt['--interactive']:
+#     AmityApp().cmdloop()
 
-print(opt)
+# print(opt)
